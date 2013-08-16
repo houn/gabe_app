81a51dd5775676882fc30ee06fa0a56894eea902
houn <good.sergio@gmail.com>
2013-08-16 12:56:21 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GabeApp::Application.config.secret_token = 'e94a8a32e8841fe5391ce697181634257c74d14d8e997b73b19f6c9c2520b7127aee7980be992bf196dbcee96c7bdb2909eaf6b978c9894d10633eb75661952a'
