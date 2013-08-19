133693c1e29611716950f3ad9c91712ad23a36d2
houn <good.sergio@gmail.com>
2013-08-19 12:46:34 +0800

    commited guardfile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let (:base_title) {"Ruby on Rails Tutorial Gabe\'s App"}
+
   describe "Home Page" do
   	it 'should have the right title' do
   		visit '/static_pages/home'
   		page.should have_selector('title', 
-  			:text => " | Home")
+  			:text => "#{base_title} | Home")
   	end
   	it 'should have the content Gabe\'s App' do
   		visit '/static_pages/home'
@@ -16,7 +19,7 @@ describe "Static Pages" do
   	it 'should have the right title' do
   		visit '/static_pages/help'
   		page.should have_selector('title', 
-  			:text => " | Help")
+  			:text => "#{base_title} | Help")
   	end
   	it 'should have the content Help' do
   		visit '/static_pages/help'
@@ -27,11 +30,22 @@ describe "Static Pages" do
   	it 'should have the right title' do
   		visit '/static_pages/about'
   		page.should have_selector('title', 
-  			:text => " | About")
+  			:text => "#{base_title} | About")
   	end
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
+  end
+  describe "Contact Page" do
+  	it 'should have the right title' do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title', 
+  			:text => "#{base_title} | Contact")
+  	end
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('h1', :text => 'Contact Us')
+  	end  	
   end
 end
