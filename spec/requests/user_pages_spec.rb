b1efa569862f25fecc2c79233913d2359fa0ac85
houn <good.sergio@gmail.com>
2013-08-25 18:05:50 +0800

    added test for user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,6 +15,11 @@ describe "User Pages" do
   		it "should not create a user" do
 			expect { click_button submit }.not_to change(User, :count)  			
   		end
+      describe "after submission" do
+        before { click_button submit } 
+        it { should have_selector('title', text: 'Sign Up')}
+        it { should have_content('error')}
+      end
   	end
   	describe "with valid information" do
   		before do
@@ -26,6 +31,12 @@ describe "User Pages" do
   		it "should create a user" do
   			expect { click_button submit }.to change(User,:count).by(1)
   		end
+      describe "after submission" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_content("Welcome") }
+      end
   	end
   end
   describe "profile page" do
