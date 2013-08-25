c553fbf5e7db428afc1d8b059b0a961dfcb6e223
houn <good.sergio@gmail.com>
2013-08-26 01:05:01 +0800

    added flash.now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			#sign the user in and redirect to the user's show page.
+		else
+			#create and error message snd re-render the signin form
+			flash.now[:error] = 'Invalid email/password combination' #Not quite right!
+			render 'new'
+		end
 	end
 	
 	def destroy
 	end
-	
+
 end
