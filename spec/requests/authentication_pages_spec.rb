efdaf261a652b6fd83e3efd56114aac04ef30fc1
houn <good.sergio@gmail.com>
2013-08-27 18:45:50 +0800

    aded authentication pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,12 +32,9 @@ describe "AuthenticationPages" do
 				fill_in "Password", with: user.password
 				click_button "Sign in"
 			end
-
-
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
-			it { should have_link('Sign in', href: signin_path) }
 		end
 	end
 end
