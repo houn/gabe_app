9726ac4b209732a296c94ba8a16f680da86e55c9
houn <good.sergio@gmail.com>
2013-08-30 15:16:42 +0800

    modfied tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,14 +34,12 @@ describe "User Pages" do
       describe "after submission" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
-        it { should have_selector('title', text: user.name) }
-        it { should have_content("Welcome") }
+        it { should have_success_message(user.name, 'Welcome') }
       end
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
-        it { should have_selector('title',text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+        it { should have_success_message(user.name, 'Welcome') }
         it { should have_link('Sign out')}
       end
       describe "followed by signout" do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,10 @@ RSpec::Matchers.define :have_error_message do |message|
 		page.should have_selector('div.alert.alert-error', text: message)
 	end
 end
+
+RSpec::Matchers.define :have_success_message do |title, message|
+	match do |page|
+		page.should have_selector('title', text: title)
+		page.should have_selector('div.alert.alert-success', text: message)
+	end
+end
