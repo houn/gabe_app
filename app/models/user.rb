8b16b899870996fffdce6169c122b7366928d9e0
houn <good.sergio@gmail.com>
2013-09-11 17:57:28 +0800

    added follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,16 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+  
   #before_save { |user| user.email = email.downcase }
+  
   before_save { self.email.downcase! }
   before_save :create_remember_token
   before_destroy :check_if_admin
@@ -30,6 +38,17 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
   private
 
   def create_remember_token
